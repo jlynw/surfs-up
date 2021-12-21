6271e551def1deb9ed5dbc4c14b25ffdb4da3313
JudyW <92497511+jlynw@users.noreply.github.com>
2021-12-21 13:32:54 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# import dependancies
+from flask import Flask
+
+# create new flask app instance
+app = Flask(__name__)
+
+# create flask routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
