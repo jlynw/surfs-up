5b194e3428c2b40c784bfbafbef64ecf893ef8c5
JudyW <92497511+jlynw@users.noreply.github.com>
2021-12-21 13:49:25 -0800

    add welcome route

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-# import dependancies
-from flask import Flask
+#import dependancies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-# create new flask app instance
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# set up database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# set base to access & query SQLite db file
+Base = automap_base()
+
+# reflect db
+Base.prepare(engine, reflect=True)
+
+# create variable for each of the classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create session link from python to db
+session = Session(engine)
+
+# set up flask & routes
 app = Flask(__name__)
 
-# create flask routes
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# create welcome route
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+
